28aed79f8480df0619337bbd5ce26970e6f1cec0
bek-app <make202006@gmail.com>
2021-04-20 13:13:31 +0600

    add a table users and balances

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,42 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">nuxtjs-pr</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <b-container class="mt-5">
+    <b-table striped hover :items="items" :fields="fields">
+      <template #cell(index)="data">
+        {{ data.index + 1 }}
+      </template>
+    </b-table>
+  </b-container>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      fields: [
+        {
+          key: 'index',
+          label: '№',
+        },
+        {
+          key: 'name',
+          label: 'Аты-жөні',
+          sortable: true,
+        },
+        {
+          key: 'balance',
+          label: 'Балансы',
+          sortable: true,
+        },
+      ],
+      items: [
+        { id: 1, name: 'Maksat', balance: 50 },
+        { id: 2, name: 'Sultan', balance: 30 },
+        { id: 5, name: 'Sultan', balance: 15 },
+        { id: 17, name: 'Sultan', balance: 20 },
+      ],
+    }
+  },
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
