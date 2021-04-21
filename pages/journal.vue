18235354ab4ed54e34d3bb5e71a3ac03743bd3d9
bek-app <make202006@gmail.com>
2021-04-21 11:09:13 +0600

    add a buttons

--- pages/journal.vue
+++ pages/journal.vue
@@ -1,10 +1,16 @@
 <template>
   <b-container class="mt-3 wrap">
     <b-table
+      responsive="sm"
       :items="items"
       :fields="fields"
       :tbody-tr-class="rowClass"
-    ></b-table>
+    >
+      <template #cell(btn)="data">
+        <b-button variant="primary" @click="added(data.item)">Қосу</b-button>
+        <b-button variant="danger" @click="removed(data.item)">Өшіру</b-button>
+      </template></b-table
+    >
   </b-container>
 </template>
 
@@ -38,6 +44,10 @@ export default {
           label: 'Статусы',
           sortable: true,
         },
+        {
+          key: 'btn',
+          label: '',
+        },
       ],
       items: [
         {
@@ -46,6 +56,7 @@ export default {
           pc: 'PC1',
           end_time: '15:30',
           status: 'success',
+          _cellVariants: { status: 'success' },
         },
         {
           name: 'Esen',
@@ -53,13 +64,15 @@ export default {
           pc: 'PC2',
           end_time: '13:10',
           status: 'busy',
+          _cellVariants: { status: 'danger' },
         },
         {
           name: 'Maks',
           start_time: '12:20',
           pc: 'PC3',
           end_time: '13:10',
-          status: '',
+          status: 'success',
+          _cellVariants: { status: 'success' },
         },
         {
           name: 'Isa',
@@ -67,6 +80,7 @@ export default {
           pc: 'PC4',
           end_time: '13:10',
           status: 'busy',
+          _cellVariants: { status: 'danger' },
         },
       ],
     }
@@ -75,6 +89,13 @@ export default {
     rowClass(item, type) {
       if (!item || type !== 'row') return
       if (item.status === 'awesome') return 'table-busy'
+    },
+
+    added(item) {
+      console.log(item.pc + ' ' + item.name)
+    },
+    removed(item) {
+      console.log(item.pc)
     },
   },
 }
